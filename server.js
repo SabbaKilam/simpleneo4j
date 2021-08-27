7d2ba6b4a0f183f090427e2f8313d8797f654664
SabbaKilam <abbas229@gmail.com>
2021-08-26 23:35:55 -0400

    9th CORS fix

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ http.createServer( ( req, res )=>{
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     */
     const headers = {
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "about:blank, *",
         "Access-Control-Allow-Credentials": "true",
         "Access-Control-Allow-Methods": "OPTIONS, GET",
         "Access-Control-Max-Age": 2592000,
