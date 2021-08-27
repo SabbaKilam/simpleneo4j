273b827e63789b594dc12a1a536f6c61d1553b7f
SabbaKilam <abbas229@gmail.com>
2021-08-26 23:53:35 -0400

    12th CORS fix

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ http.createServer( ( req, res )=>{
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     */
     const headers = {
-        "Access-Control-Allow-Origin": "https://abbas411.com/",
+        "Access-Control-Allow-Origin": "https://kin-keepers-neo4j.herokuapp.com/",
         "Access-Control-Allow-Credentials": "true",
         "Access-Control-Allow-Methods": "OPTIONS, GET",
         "Access-Control-Max-Age": 2592000,
