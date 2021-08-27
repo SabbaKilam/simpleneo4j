dee2d30747f84af30963872a133b9bab74bbf34f
SabbaKilam <abbas229@gmail.com>
2021-08-26 23:15:16 -0400

    8th CORS fix Access-Control-Allow-Headers: *

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ http.createServer( ( req, res )=>{
         "Access-Control-Allow-Credentials": "true",
         "Access-Control-Allow-Methods": "OPTIONS, GET",
         "Access-Control-Max-Age": 2592000,
-        "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+        "Access-Control-Allow-Headers": "*"
     };
     if (req.method === "OPTIONS") {
       res.writeHead(204, headers);
