6700fa2cf8f9d84f6f4d16a58b9e6e3e9b0d88ad
SabbaKilam <abbas229@gmail.com>
2022-01-24 14:02:26 -0500

    h.deleteOneMember - 5

--- api_methods.js
+++ api_methods.js
@@ -306,7 +306,7 @@ module.exports = {
 
         try {
             const result = await session.run(
-                `MATCH ( s:Person {email: "${email}"} ) DETACH DELETE s MATCH(p) RETURN p`
+                `MATCH ( s:Person {email: "${email}"} ) DETACH DELETE s`
             )        
             const singleRecord = result.records[0]
             const node = singleRecord.get(0)
