fc58a2cdca12bd92d1ab4b226228963a1f3d9ec3
SabbaKilam <abbas229@gmail.com>
2022-03-01 15:29:45 -0500

    generateFamID server test v.0.4.81

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ http.createServer( ( req, res )=>{
     //allow cors:
     CorsMiddleware( req, res );
 
-    res.writeHead( 200, {'Content-Type', 'text/plain'});
+    res.writeHead( 200, {'Content-Type': 'text/plain'});
     res.write( generateFamID() );
 
     let url = decodeURI(req.url)
